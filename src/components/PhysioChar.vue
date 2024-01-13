b4ae5743c6b0fd9f54df74f1a63d72e16f1d9965
WuMianzhi <sumangde@gmail.com>
2024-01-13 11:48:07 +0800

    feat: 优化了生理特征

--- src/components/PhysioChar.vue
+++ src/components/PhysioChar.vue
@@ -1,30 +1,31 @@
 <template>
   <div>
-    <h1 class="text-4xl"> 生理特征</h1>
+    <h1 class="text-4xl main-title"> 生理特征</h1>
     <div class="flex justify-center flex-col">
-      <div class="flex justify-between ">
+      <div class="flex justify-between subtitle">
         <p>阴道</p>
         <p>未分化</p>
         <p>阴茎</p>
       </div>
-      <div>
-        <input type="range" name="" id="" class="w-full">
+      <div class="py-3">
+        <input type="range" name="" id="" class="w-full ring-4 ring-white/30 rounded-full h-5 rainbow-bg box-content">
       </div>
-      <img src="../assets/gender.svg" alt="">
+      <img src="../assets/gender.svg" alt="" class="mb-3">
     </div>
     <div>
-      <div v-for="(hormone, index) in hormones" class="flex">
-        <div>图片</div>
+      <div v-for="(hormone, index) in hormones" class="flex mb-3">
+        <div>
+          <img :src="hormone.pic" :alt="hormone.name" srcset="">
+        </div>
         <div class="flex-1">
           <div class="flex justify-between ">
             <template v-for="i in 20" :key="i">
               <button @click="modVal(index, i * 5)"
                 :style="{ 'background-color': calInterColorByHex(hormone.colorSeries[0], hormone.colorSeries[1], i) }"
-                :class="['h-4 w-2 p-0 b-0 bg-gray-900 step-btn transition-all', i * 5 == hormone.value ? 'relative bottom-2' : '']"></button>
+                :class="['score-btn', i * 5 == hormone.value ? 'relative bottom-2' : '']"></button>
             </template>
           </div>
-          <p>{{ hormone.name }}</p>
-
+          <p class="subtitle text-left">{{ hormone.name }}</p>
         </div>
       </div>
     </div>
@@ -32,28 +33,37 @@
 </template>
 
 <script setup lang="ts">
+import progesterone from '../assets/progesterone.svg';
+import androgens from '../assets/androgens.svg'
+import estrogen from '../assets/estrogen.svg'
+import { reactive } from 'vue';
+
 const emit = defineEmits<{
   selfChange: [index: number, value: number];
   updateVal: [index: number, value: number];
 }>();
 
-const hormones = [
+const hormones = reactive([
   {
     name: '雌激素',
     colorSeries: ["ff9292", "ffd6a7"],
     value: 50,
+    pic: estrogen
   }, {
     name: '雄激素/睾酮',
-    colorSeries: ["ff9292", "ffd6a7"],
+    colorSeries: ["ffd6a7", "fffb8f"],
     value: 50,
+    pic: androgens
   }, {
     name: '孕激素',
-    colorSeries: ["ff9292", "ffd6a7"],
+    colorSeries: ["fffb8f", "c9ffaf"],
     value: 50,
+    pic: progesterone
   }
-]
+])
 
 const modVal = (index: number, newVal: number) => {
+  hormones[index].value = newVal;
   emit('updateVal', index, newVal)
 }
 
@@ -74,4 +84,21 @@ const hex2RGB = (hex: string) => {
 
 </script>
 
-<style scoped></style>
+<style scoped>
+input::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  width: 16px;
+  height: 16px;
+  border-radius: 50%;
+  background: #eeeeee;
+  background-image:
+    linear-gradient(140.55deg, rgba(216, 235, 255, 0.2) 1.7%, rgba(255, 234, 253, 0.199136) 44%, rgba(254, 255, 218, 0.1979) 103.46%),
+    radial-gradient(50% 50% at 50% 50%, #FFFFFF 43.23%, rgba(255, 255, 255, 0) 100%),
+    linear-gradient(180deg, #FFFFFF 0%, rgba(255, 255, 255, 0) 100%);
+  ;
+  cursor: pointer;
+  border: 2px solid #ffffff50;
+  box-sizing: border-box;
+  box-shadow: 0px 0 0 4px #cbcbcb7a;
+}
+</style>
