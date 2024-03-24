b3fc2f3cbdee15be648af60bbf19143bc8851515
Orikes <st.eve.orikes@gmail.com>
2024-03-24 15:13:58 +0800

    i18n: sexual identity

--- src/locales/setupI18n.ts
+++ src/locales/setupI18n.ts
@@ -5,15 +5,39 @@ const messages = {
     message: {
       hello: "hello world",
     },
+    sexualIdentity: {
+      identity: "Sexual Identity",
+      queer: "queer",
+      pansexual: "pansexual",
+      homosexual: "homosexual",
+      heterosexual: "heterosexual",
+      asexual: "asexual",
+    },
   },
   ja: {
     message: {
       hello: "こんにちは、世界",
     },
+    sexualIdentity: {
+      identity: "性的アイデンティティ", //not sure
+      queer: "クィア",
+      pansexual: "パンセクシュアル（全性愛）",
+      homosexual: "ホモセクシュアル（同性愛）",
+      heterosexual: "バイセクシュアル（両性愛）",
+      asexual: "アセクシュアル（無性愛）",
+    },
   },
   "zh-CN": {
     message: {
       hello: "你好",
+    },
+    sexualIdentity: {
+      identity: "性/情欲倾向的认同/身份",
+      queer: "酷儿",
+      pansexual: "泛性恋",
+      homosexual: "同性恋",
+      heterosexual: "异性恋",
+      asexual: "无性恋",
     },
   },
 };
--- src/pages/TFGU/sIdentity/SId.ts
+++ src/pages/TFGU/sIdentity/SId.ts
@@ -1,13 +1,30 @@
 import { reactive } from "vue";
 import { GenderGroup } from "../../../types/index";
+import { setupI18n } from "../../../locales/setupI18n";
 
 export const sexualIdentity: GenderGroup = reactive({
-  title: "性/情欲倾向的认同/身份",
+  title: setupI18n.global.t("sexualIdentity.identity"),
   category: [
-    { name: "酷儿", colorSeries: ["ff9292", "ffd6a7"], value: 50 },
-    { name: "双性恋", colorSeries: ["ffd6a7", "fffb8f"], value: 50 },
-    { name: "同性恋", colorSeries: ["fffb8f", "c9ffaf"], value: 50 },
-    { name: "异性恋", colorSeries: ["c9ffaf", "90f8ff"], value: 50 },
+    {
+      name: setupI18n.global.t("sexualIdentity.queer"),
+      colorSeries: ["ff9292", "ffd6a7"],
+      value: 50,
+    },
+    {
+      name: setupI18n.global.t("sexualIdentity.pansexual"),
+      colorSeries: ["ffd6a7", "fffb8f"],
+      value: 50,
+    },
+    {
+      name: setupI18n.global.t("sexualIdentity.homosexual"),
+      colorSeries: ["fffb8f", "c9ffaf"],
+      value: 50,
+    },
+    {
+      name: setupI18n.global.t("sexualIdentity.heterosexual"),
+      colorSeries: ["c9ffaf", "90f8ff"],
+      value: 50,
+    },
     { name: "customer", colorSeries: ["90f8ff", "9390ff"], value: 50 },
   ],
   mainColor: "fuchsia",
